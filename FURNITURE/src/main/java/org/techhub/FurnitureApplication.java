3558aa1d50f506a73c5796845440507bd8dafdd4
TECH HUB <giri.techhub23@gmail.com>
2020-08-10 17:10:35 +0530

    this is the first without any code

---
+++ FURNITURE/src/main/java/org/techhub/FurnitureApplication.java
@@ -0,0 +1,13 @@
+package org.techhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureApplication.class, args);
+	}
+
+}
